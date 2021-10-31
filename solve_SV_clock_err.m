c5f3b9889b743e7dd26e67ab543c09e6d22245f1
yckan <36065223+yckan@users.noreply.github.com>
2021-11-01 03:21:23 +0800

    Update solve_SV_clock_err.m

--- solve_SV_clock_err.m
+++ solve_SV_clock_err.m
@@ -1,3 +1,12 @@
+% Input a - semimajor axis
+% Input e - eccentricity
+% Input E_k - eccentric anomaly
+% Input af0 - clock bias correction coefficient (s)
+% Input af1 - clock drift correction coefficient (s/s)
+% Input af2 - frequency drift correction coefficient (s/s/s)
+% Input t_s - satellite transmission time (sec)
+% Input toc - clock data reference time (sec)
+
 function Delta_t_SV = solve_SV_clock_err(a,e,E_k,af0,af1,af2,t_s,toc)
 
     F = -4.442807633e-10; %Relativistic correction term constant
