84589cb484970f51d430cd42aaecdf8b46d9ef37
yckan <36065223+yckan@users.noreply.github.com>
2021-11-01 03:16:09 +0800

    Update AAE6102_Assignment_main.m

--- AAE6102_Assignment_main.m
+++ AAE6102_Assignment_main.m
@@ -2,9 +2,9 @@ clear all; clc; close all
 
 [x_s,y_s,z_s,a,e,af0,af1,af2,E_k,t_s,toc,pr_rcvr] = ...
     solve_SV_pos('/media/pnl/Shared_Data/AAE6102/Assignment/Assignment/Data/rcvr.dat', ...
-    '/media/pnl/Shared_Data/AAE6102/Assignment/Assignment/Data/eph.dat');
+    '/media/pnl/Shared_Data/AAE6102/Assignment/Assignment/Data/eph.dat'); % Required to change path for reading eph.dat and rcvr.dat
 
-Delta_t_SV = solve_SV_clock_err(a,e,E_k,af0,af1,af2,t_s,toc);
+Delta_t_SV = solve_SV_clock_err(a,e,E_k,af0,af1,af2,t_s,toc); % Calculate Satellite clock error
 
-x0 = [-2694685.473;-4293642.366;3857878.924;0.0];
-[x_u] = solve_x_user_LS(pr_rcvr,x0,x_s,y_s,z_s,Delta_t_SV);
+x0 = [-2694685.473;-4293642.366;3857878.924;0.0]; % Initialize user position for Least Square Method
+[x_u] = solve_x_user_LS(pr_rcvr,x0,x_s,y_s,z_s,Delta_t_SV); % Solve user position and clock bias by LS method
